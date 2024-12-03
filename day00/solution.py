4037743a131489a2982a546d2f953cbd0e5f286f
Felicity Matthews <felicitymatthews30@gmail.com>
2024-12-03 21:41:19 +1100

    add template

---
+++ day00/solution.py
@@ -0,0 +1,35 @@
+PART_ONE_SAMPLE_ANSWER = 0
+PART_TWO_SAMPLE_ANSWER = 0
+SAMPLE_FILENAME = "sample.txt"
+REAL_DATA_FILENAME = "data.txt"
+
+def compute_answer(filename: str, func, answer: int = None):
+    with open(filename, 'r') as f:
+        lines = [line.strip() for line in f]
+    
+    res = func(lines)
+
+    if answer is not None:
+        print("sample answer is:", res)
+        assert res == answer
+
+    return res
+
+
+def part_one_code(lines: list):
+    pass
+
+
+def part_two_code(lines: list):
+    pass
+
+
+if __name__ == '__main__':
+    print('----- part one -----')
+    compute_answer(SAMPLE_FILENAME, part_one_code, PART_ONE_SAMPLE_ANSWER)
+    print("part one answer:", compute_answer(REAL_DATA_FILENAME, part_one_code))
+    print('----- part two -----')
+    compute_answer(SAMPLE_FILENAME, part_two_code, PART_TWO_SAMPLE_ANSWER)
+    print("part one answer:", compute_answer(REAL_DATA_FILENAME, part_two_code))
+    print('--------------------')
+
